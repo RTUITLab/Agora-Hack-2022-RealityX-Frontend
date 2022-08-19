72e908a1ebd37e11189a48036f84243561849cf2
IvLaptev <ivlaptev13@ya.ru>
2022-08-19 21:37:46 +0300

    Added login guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import IUser from '@/store/User'
 
 Vue.use(VueRouter)
 
@@ -11,11 +13,13 @@ const routes: Array<RouteConfig> = [
     component: HomeView
   },
   {
+    path: '/login',
+    name: 'login',
+    component: LoginView
+  },
+  {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
@@ -24,6 +28,14 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  const user = sessionStorage.getItem('user')
+
+  if (!user && to.name !== 'login') {
+    next('/login')
+  } else next()
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>Login form</div>
+    <div>Image</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class LoginView extends Vue {
+
+}
+</script>
+
+<style scoped>
+
+</style>
