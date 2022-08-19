135f69bc0a40a3edf42684aa56c2716d84279bd5
IvLaptev <ivlaptev13@ya.ru>
2022-08-20 02:08:26 +0300

    Added data model to header widget

---
+++ src/store/types/HeaderWidget.ts
@@ -0,0 +1,17 @@
+import IWidget from '@/store/types/Widget'
+
+export interface ILink {
+  url: string,
+  name: string
+}
+
+export interface IHeaderWidget extends IWidget {
+  data: {
+    height: number,
+    marginBottom: number,
+    logoUrl: string,
+    titleColor: string,
+    linksColor: string,
+    links: Array<ILink>
+  }
+}
