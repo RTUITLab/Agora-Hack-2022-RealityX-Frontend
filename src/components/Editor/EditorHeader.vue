19b06dc280a6de5ad89acadc45063ced05a2d81d
IvLaptev <ivlaptev13@ya.ru>
2022-08-20 01:36:48 +0300

    Changed editor header

--- src/components/Editor/EditorHeader.vue
+++ src/components/Editor/EditorHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <header>
     <h1>Site title</h1>
+    <button class="publish blue-btn">Опубликовать <img src="../../assets/Check.svg"></button>
   </header>
 </template>
 
@@ -15,15 +16,31 @@ export default class EditorHeader extends Vue {
 
 <style lang="scss" scoped>
 header {
-  padding: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 32px 40px;
+
   box-sizing: border-box;
   border-bottom: 1px solid #E2E8F0;
 
   h1 {
-    margin: 0;
+    margin: 0 auto 0 0;
 
     font-size: 20px;
     font-weight: 600;
+  }
+
+  .publish {
+    width: max-content;
+    padding: 12px 14px;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-left: 8px;
+    }
   }
 }
 </style>
---
+++ src/components/Editor/EditorWorkspace.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'EditorWorkspace'
+}
+</script>
+
+<style scoped>
+
+</style>
