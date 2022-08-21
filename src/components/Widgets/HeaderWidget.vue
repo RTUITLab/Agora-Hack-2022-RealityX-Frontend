70d62c750945985a07c0002191bdedd36c12068c
IvLaptev <ivlaptev13@ya.ru>
2022-08-21 06:42:02 +0300

    Added header template generation

--- src/components/Widgets/HeaderWidget.vue
+++ src/components/Widgets/HeaderWidget.vue
@@ -27,49 +27,6 @@ export default class HeaderWidget extends Vue {
 
     this.$watch('inputData', (newData) => (this.data = newData))
   }
-
-  public genTemplate () {
-    const style = `
-    <style>
-    .ws-header {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-
-      padding: 40px 80px;
-      box-sizing: border-box;
-    }
-    @media (max-width: 678px) {
-      .ws-header {
-        flex-direction: column;
-        gap: 16px;
-        height: auto !important;
-      }
-
-      .ws-header_link {
-        display: block;
-        margin: 8px auto;
-      }
-
-      .ws-header_logo-wrapper {
-        margin: auto;
-      }
-    }
-    </style>
-    `
-    return style + `<div id="${this.data!.id} + '_' + ${this.data!.number}" class="ws-header" style="height: ${this.data!.data.height}px; margin-bottom: ${this.data!.data.marginBottom}px">
-    <div style="margin-right: auto;">
-      ${this.data!.data.logoUrl ? `<img v-if="data.data.logoUrl" height="${this.data!.data.height - 40 * 2}px" src="${this.data!.data.logoUrl}" alt="Site logo">` : ''}
-    </div>
-    <ul class="ws-header_links" style="padding: 0; margin: 0">
-      ${this.data!.data.links.map((link) =>
-        `<li style="list-style: none; display: inline-block; margin-left: 32px">
-        <a href="${link.url}" style="text-decoration: none; font-size: 18px; color: ${this.data!.data.linksColor}">${link.name}</a>
-      </li>`
-    ).join('')}
-    </ul>
-  </div>`
-  }
 }
 </script>
 
--- src/store/types/HeaderWidget.ts
+++ src/store/types/HeaderWidget.ts
@@ -44,3 +44,60 @@ export function createDefaultHeader (): IHeaderWidget {
     template: ''
   }
 }
+
+export function staticHeaderTemplate (): string {
+  return `
+  <style>
+  .ws-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    padding: 40px 80px;
+    box-sizing: border-box;
+  }
+
+  .ws-header_logo-wrapper {
+    margin-right: auto;
+  }
+
+  .ws-header_link {
+    list-style: none;
+    display: inline-block;
+    margin-left: 32px;
+  }
+
+  @media (max-width: 1042px) {
+    .ws-header {
+      flex-direction: column;
+      gap: 16px;
+      height: auto !important;
+    }
+
+    .ws-header_logo-wrapper {
+      margin: auto;
+    }
+
+    .ws-header_link {
+      display: block;
+      margin: 8px auto;
+    }
+  }
+  </style>
+  `
+}
+
+export function headerToTemplate (data: IHeaderWidget): string {
+  return `<div id="${data.id} + '_' + ${data.number}" class="ws-header" style="height: ${data.data.height}px; margin-bottom: ${data.data.marginBottom}px; background: ${data.data.background}">
+    <div class="ws-header_logo-wrapper">
+      ${data.data.logoUrl ? `<img height="${data.data.height - 40 * 2}px" src="${data.data.logoUrl}" alt="Site logo">` : ''}
+    </div>
+    <ul class="ws-header_links" style="padding: 0; margin: 0">
+      ${data.data.links.map((link) => `
+      <li class="ws-header_link">
+        <a href="${link.url}" style="text-decoration: none; font-size: 18px; color: ${data.data.linksColor}">${link.name}</a>
+      </li>
+      `).join('')}
+    </ul>
+  </div>`
+}
